c0cc97a58a0712c2a6ee4d514cbe910ede30ab91
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2022-05-11 23:12:49 +0900

    contact 작업, 인스타 링크
    인스타 링크는 새탭에 열리게 셋팅

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -7,37 +7,6 @@ const About: NextPage = () => {
   return (
     <div>
       <Nevigator />
-      <div className="max-w-2xl mx-auto py-10 px-4 grid items-center grid-cols-1 gap-y-4 gap-x-8 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
-        <div>
-          <div className="py-6 text-gray-800 font-bold text-xl">김나미</div>
-          <div className="py-3 text-gray-700 font-medium text-l">화가</div>
-        </div>
-        <div className="flex-col pt-20 py-6">
-          <div className="flex">
-          <div className="mr-1 text-gray-700 font-medium">e-mail</div>
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
-          </div>
-          <div className="mb-4 text-gray-800 font-semibold text-l">treenami@naver.com</div>
-          
-          <div className="flex">
-          <div className="mr-1 text-gray-700 font-medium">phone </div>
-          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm3 14a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path></svg>
-          </div>
-          <div className="mb-4 text-gray-800 font-semibold text-l">010-3375-1463</div>
-          
-          <div className="flex">
-          <div className="mr-1 text-gray-700 font-medium">instagram </div>
-          <Image
-                src="/instagram.png"
-                alt="Picture of the author"
-                width={20}
-                height={20}
-              />
-            </div>
-          <div className="text-gray-800 font-semibold text-l cursor-pointer">@treenami_studio</div>
-
-        </div>
-      </div>
       </div>
   );
 };
