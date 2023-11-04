6994bee5b40aeb7b1113d7dadc3d9c24550c3898
Yoon79 <93427321+Yoon79@users.noreply.github.com>
2023-11-04 16:19:54 +0900

    2회 전시내용추가
    임의로 해봄

--- pages/exhibitions/index.tsx
+++ pages/exhibitions/index.tsx
@@ -1,13 +1,35 @@
-import type { NextPage } from "next";
-import Link from "next/link";
-import Nevigator from "../components/navigator";
-import Image from "next/image";
-import Footer from "../components/footer";
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import Nevigator from '../components/navigator';
+import Image from 'next/image';
+import Footer from '../components/footer';
 
 const Exhibitions: NextPage = () => {
   return (
     <div>
       <Nevigator />
+      <div className="max-w-2xl mx-auto py-10 px-4 grid items-center grid-cols-1 gap-y-4 gap-x-8 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
+        <div className="lg:ml-56  xl:ml-64">
+          <Image
+            src="/cuty_love.jpg"
+            alt="Picture of the author"
+            width={430}
+            height={335}
+          />
+        </div>
+        <div className="flex-col pt-5 py-6">
+          <div className="font-['font-AlegreyaSans'] font-bold text-gray-800 text-xl">
+            시선
+          </div>
+          <div className="ml-2 mt-0.5 text-gray-700 font-medium">
+            제2회 김나미 개인전
+          </div>
+          <div className="flex-col pt-5 py-6">
+            <div>카페 ㅇㅇㅇ</div>
+            <div>2023.12.01 - 06.06 임의로 넣음</div>
+          </div>
+        </div>
+      </div>
       <div className="max-w-2xl mx-auto py-10 px-4 grid items-center grid-cols-1 gap-y-4 gap-x-8 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid-cols-2">
         <div className="lg:ml-56  xl:ml-64">
           <Image
