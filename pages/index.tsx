b37913551df5d12f2eeada5cd22f022c6f5d3aa0
Minhyuk <minhyukit@gmail.com>
2023-11-10 23:12:24 +0900

    index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,8 @@
-import type { NextPage } from 'next';
-import Link from 'next/link';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import { useEffect } from 'react';
-import Map from './components/map';
-import {
-  AnnotationIcon,
-  GlobeAltIcon,
-  LightningBoltIcon,
-  ScaleIcon,
-} from '@heroicons/react/outline';
+import type {NextPage} from 'next';
 import Nevigator from './components/navigator';
 import Main from './components/main';
 import Detail from './components/detail';
 import Footer from './components/footer';
-import Place from './components/place';
 
 /* This example requires Tailwind CSS v2.0+ */
 
