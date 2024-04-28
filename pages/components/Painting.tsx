edcba5e986ef914832ff089756d903cece7bb7c2
Minhyuk <42267759+minhyukwang@users.noreply.github.com>
2024-04-28 16:19:56 +0900

    Painting.tsx

--- pages/components/Painting.tsx
+++ pages/components/Painting.tsx
@@ -1,7 +1,6 @@
-import {useState} from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {NextPage} from "next";
 import Skeleton from "react-loading-skeleton";
 
 type Props = {
@@ -10,35 +9,38 @@ type Props = {
     href: string;
 };
 
-const Painting: NextPage<Props> = ({name, imageSrc, href}) => {
+const Painting: React.FC<Props> = ({ name, imageSrc, href }) => {
     const [isLoading, setIsLoading] = useState(true);
 
     const handleImageLoad = () => {
         setIsLoading(false);
     };
 
+    // 만약 필수 속성이 전달되지 않은 경우를 처리합니다.
+    // if (!name || !imageSrc || !href) {
+    //     return <div>필수 속성이 누락되었습니다.</div>;
+    // }
+
     return (
-        <div
-            className="relative w-auto h-auto bg-white cursor-pointer overflow-hidden sm:aspect-w-2 lg:aspect-w-1 lg:aspect-h-1">
-            {isLoading ? <Skeleton width={500}
-                                   height={420}/> :
-                <Link href={href} key={name}>
-                    <a>
-                        {/* Use the 'Image' component from 'next/optimized-images' */}
-                        <Image
-                            src={imageSrc}
-                            alt=""
-                            width={500}
-                            height={420}
-                            // sizes="100vw"
-                            style={{display: isLoading ? 'none' : 'block'}}
-                            objectFit="contain"
-                            onLoad={handleImageLoad}
-                            // Other props as needed
-                        />
-                    </a>
-                </Link>
-            }
+        <div className="relative w-auto h-auto bg-white cursor-pointer overflow-hidden sm:aspect-w-2 lg:aspect-w-1 lg:aspect-h-1">
+            {isLoading && <Skeleton width={500} height={420} />}
+            {/* href와 name이 제대로 전달되었는지 확인한 후 링크를 렌더링합니다. */}
+            <Link href={href ?? ""} passHref>
+                <a>
+                    {/* Use the 'Image' component from 'next/optimized-images' */}
+                    <Image
+                        src={imageSrc ?? ""}
+                        alt=""
+                        width={500}
+                        height={420}
+                        // sizes="100vw"
+                        style={{ display: isLoading ? "none" : "block" }}
+                        objectFit="contain"
+                        onLoad={handleImageLoad}
+                        // Other props as needed
+                    />
+                </a>
+            </Link>
         </div>
     );
 };
