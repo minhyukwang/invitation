6f72de589e66893488a87517aee031217160cf3e
Minhyuk <42267759+minhyukwang@users.noreply.github.com>
2024-04-28 16:10:23 +0900

    Painting.tsx

--- pages/components/Painting.tsx
+++ pages/components/Painting.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
+import {useState} from "react";
 import Link from "next/link";
 import Image from "next/image";
 import {NextPage} from "next";
 import Skeleton from "react-loading-skeleton";
-
-
 
 type Props = {
     name: string;
@@ -12,7 +10,7 @@ type Props = {
     href: string;
 };
 
-const Painting: NextPage<Props> = ({ name,imageSrc, href  }) => {
+const Painting: NextPage<Props> = ({name, imageSrc, href}) => {
     const [isLoading, setIsLoading] = useState(true);
 
     const handleImageLoad = () => {
@@ -20,26 +18,27 @@ const Painting: NextPage<Props> = ({ name,imageSrc, href  }) => {
     };
 
     return (
-    <div
-        className="relative w-auto h-auto bg-white cursor-pointer overflow-hidden sm:aspect-w-2 lg:aspect-w-1 lg:aspect-h-1">
-        {isLoading && <Skeleton width={500}
-                                height={420}/>}
-        <Link href={href} key={name}>
-            <a>
-                {/* Use the 'Image' component from 'next/optimized-images' */}
-                <Image
-                    src={imageSrc}
-                    alt=""
-                    width={500}
-                    height={420}
-                    // sizes="100vw"
-                    style={{display: isLoading ? 'none' : 'block'}}
-                    objectFit="contain"
-                    onLoad={handleImageLoad}
-                        // Other props as needed
-                    />
-                </a>
-            </Link>
+        <div
+            className="relative w-auto h-auto bg-white cursor-pointer overflow-hidden sm:aspect-w-2 lg:aspect-w-1 lg:aspect-h-1">
+            {isLoading ? <Skeleton width={500}
+                                   height={420}/> :
+                <Link href={href} key={name}>
+                    <a>
+                        {/* Use the 'Image' component from 'next/optimized-images' */}
+                        <Image
+                            src={imageSrc}
+                            alt=""
+                            width={500}
+                            height={420}
+                            // sizes="100vw"
+                            style={{display: isLoading ? 'none' : 'block'}}
+                            objectFit="contain"
+                            onLoad={handleImageLoad}
+                            // Other props as needed
+                        />
+                    </a>
+                </Link>
+            }
         </div>
     );
 };
